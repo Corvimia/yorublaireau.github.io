2937ae265c23bd97caec1f4f3a42c2932621f454
yorublaireau <yorublaireau@users.noreply.github.com>
2020-08-07 19:45:34 +0000

    deploy: 814c7bc6f005e31b75f9613cf75f97b3b613b8c3

---
+++ precache-manifest.db40c23d55c2fc3d03e06be82fad030e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69dba4e8b8b7517974183d9456502594",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33634b00ee0ce6313c95",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7498b0fade7bdf140e09",
+    "url": "/static/js/2.b6290089.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b6290089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33634b00ee0ce6313c95",
+    "url": "/static/js/main.1bfe2678.chunk.js"
+  },
+  {
+    "revision": "030ee12fe3e5a24bcfd2",
+    "url": "/static/js/runtime-main.f8942203.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
